d7fa32f70309053507cca0115118ccfa0897304b
sudhark07 <49035605+sudhark07@users.noreply.github.com>
2024-08-20 14:27:36 +0530

    Update PrintColorCode.Tests.cpp

--- PrintColorCode.Tests.cpp
+++ PrintColorCode.Tests.cpp
@@ -1,1 +1,10 @@
 #include "PrintColorCode.h"
+#include <gtest/gtest.h>
+
+TEST(PrintColorCodeTestSuite, AssertColorCodeManualItemsCount) {
+    // Arrange
+        int expectedCount = 25;
+    // Act
+        int actualCount = printColorMap();
+    // Assert
+        ASSERT_EQ(expectedCount, actualCount);
