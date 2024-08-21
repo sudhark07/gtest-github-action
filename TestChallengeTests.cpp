ccb8c00476cfa0e081175e9d11cb22424f1aaea2
sudhark07 <49035605+sudhark07@users.noreply.github.com>
2024-08-21 15:40:06 +0530

    Update TestChallengeTests.cpp

--- TestChallengeTests.cpp
+++ TestChallengeTests.cpp
@@ -1,13 +1,32 @@
 #include <gtest/gtest.h>
 #include "TestChallenge.h"
 
-extern int alertFailureCount = 0;
-TEST(test_challenge_test_alert, check_return_value_200) {
+extern int alertFailureCount;
+void resetAlertFailureCount() {
+    alertFailureCount = 0;
+}
+
+TEST(test_challenge_test_alert, check_alertCount_1) {
     //arrange
     float input = 82.0;
     float expectedOutput = 1;
+    resetAlertFailureCount();
     // act
     alertInCelcius(input);
     // assert
     ASSERT_GE( alertFailureCount, expectedOutput);
 }
+
+TEST(test_challenge_test_alert, check_alertCount_2) {
+    //arrange
+    float input = 100.0;
+    float expectedOutput = 2;
+    resetAlertFailureCount();
+    // act
+    alertInCelcius(input);
+    alertInCelcius(input);
+    // assert
+    ASSERT_GE( alertFailureCount, expectedOutput);
+}
+
+
